4c434a09434426b1a75ee217053d09a046011bb8
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-07-31 09:50:34 +0530

    Number button clicks logic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Calculator
 {
@@ -20,9 +8,66 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SelectedOperator? _selectedOperator = null;
+        private double calculatedResult = 0.0;
         public MainWindow()
         {
             InitializeComponent();
+            SetDefaults();
         }
+
+        private void SetDefaults()
+        {
+            ResultLbl.Content = string.Empty;
+            ExpressionLbl.Content = string.Empty;
+        }
+
+
+        private void AcBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            SetDefaults();
+        }
+
+        private void NegativeBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void PercentageBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void EqualBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void DotBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnOperationBtnClick(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void OnNumberBtnClick(object sender, RoutedEventArgs e)
+        {
+            var btnContent = ((Button)sender).Content.ToString();
+            if (int.TryParse(btnContent, out var selectedNumber))
+            {
+
+                ResultLbl.Content = $"{ResultLbl.Content}{selectedNumber}";
+            }
+
+        }
+    }
+
+    public enum SelectedOperator
+    {
+        Addition,
+        Subtract,
+        Multiply,
+        Divide
     }
 }
