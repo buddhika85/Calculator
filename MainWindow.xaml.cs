c30d3c763618b8dee45c6c340c14da9c057eebd8
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-08-01 21:25:05 +0530

    key presses

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Calculator
 {
@@ -15,6 +16,79 @@ namespace Calculator
         {
             InitializeComponent();
             SetDefaults();
+            KeyDown += HandleKeyPress;
+        }
+
+        private void HandleKeyPress(object sender, KeyEventArgs e)
+        {
+            int? numberPressed = null;
+            string? operationPressed = null;
+            switch (e.Key)
+            {
+                case Key.NumPad0:
+                case Key.D0:
+                    numberPressed = 0;
+                    break;
+                case Key.NumPad1:
+                case Key.D1:
+                    numberPressed = 1;
+                    break;
+                case Key.NumPad2:
+                case Key.D2:
+                    numberPressed = 2;
+                    break;
+                case Key.NumPad3:
+                case Key.D3:
+                    numberPressed = 3;
+                    break;
+                case Key.NumPad4:
+                case Key.D4:
+                    numberPressed = 4;
+                    break;
+                case Key.NumPad5:
+                case Key.D5:
+                    numberPressed = 5;
+                    break;
+                case Key.NumPad6:
+                case Key.D6:
+                    numberPressed = 6;
+                    break;
+                case Key.NumPad7:
+                case Key.D7:
+                    numberPressed = 7;
+                    break;
+                case Key.NumPad8:
+                case Key.D8:
+                    numberPressed = 8;
+                    break;
+                case Key.NumPad9:
+                case Key.D9:
+                    numberPressed = 9;
+                    break;
+                case Key.Add:
+                    operationPressed = "+";
+                    break;
+                case Key.Subtract:
+                    operationPressed = "-";
+                    break;
+                case Key.Multiply:
+                    operationPressed = "*";
+                    break;
+                case Key.Divide:
+                    operationPressed = "/";
+                    break;
+                case Key.Enter:
+                    operationPressed = "=";
+                    break;
+                    //case Key.Decimal:
+                    //    operationPressed = ".";
+                    //    break;
+            }
+
+            if (numberPressed != null)
+                OnNumberInput(numberPressed.Value);
+            else if (operationPressed != null)
+                OnOperationInput(operationPressed);
         }
 
         private void SetDefaults()
@@ -59,16 +133,7 @@ namespace Calculator
         private void OnOperationBtnClick(object sender, RoutedEventArgs e)
         {
             var operation = GetBtnContent(sender);
-            if (_selectedOperator != SelectedOperator.Nothing)
-            {
-                ExpressionLbl.Content = $"{ExpressionLbl.Content}{ResultLbl.Content}{operation}";
-                PerformOperation();     // previous calculation
-            }
-            else
-            {
-                ExpressionLbl.Content = $"{ResultLbl.Content}{operation}";
-            }
-            SetSelectedOperator(operation);
+            OnOperationInput(operation);
         }
 
         private void OnNumberBtnClick(object sender, RoutedEventArgs e)
@@ -76,28 +141,54 @@ namespace Calculator
             var numberStr = GetBtnContent(sender);
             if (int.TryParse(numberStr, out var selectedNumber))
             {
-                if (_selectedOperator != SelectedOperator.Nothing)
+                OnNumberInput(selectedNumber);
+            }
+        }
+
+        /// <summary>
+        /// used to manage operation key presses from GUI and keyboard
+        /// </summary>
+        /// <param name="operation"></param>
+        private void OnOperationInput(string? operation)
+        {
+            if (_selectedOperator != SelectedOperator.Nothing)
+            {
+                ExpressionLbl.Content = $"{ExpressionLbl.Content}{ResultLbl.Content}{operation}";
+                PerformOperation(); // previous calculation
+            }
+            else
+            {
+                ExpressionLbl.Content = $"{ResultLbl.Content}{operation}";
+            }
+
+            SetSelectedOperator(operation);
+        }
+
+        /// <summary>
+        /// used to manage number key presses from GUI and keyboard
+        /// </summary>
+        /// <param name="selectedNumber"></param>
+        private void OnNumberInput(int selectedNumber)
+        {
+            if (_selectedOperator != SelectedOperator.Nothing)
+            {
+                if (_calculatedValue == 0.0)
                 {
-                    if (_calculatedValue == 0.0)
-                    {
-                        double.TryParse(ResultLbl.Content.ToString(), out _calculatedValue);
-                        ResultLbl.Content = $"{selectedNumber}";
-                    }
-                    else
-                    {
-                        if (_selectedOperator == SelectedOperator.Nothing)
-                            ResultLbl.Content = $"{ResultLbl.Content}{selectedNumber}";
-                        else
-                            ResultLbl.Content = $"{selectedNumber}";
-                    }
+                    double.TryParse(ResultLbl.Content.ToString(), out _calculatedValue);
+                    ResultLbl.Content = $"{selectedNumber}";
                 }
                 else
                 {
-                    ResultLbl.Content = $"{ResultLbl.Content}{selectedNumber}";
+                    if (_selectedOperator == SelectedOperator.Nothing)
+                        ResultLbl.Content = $"{ResultLbl.Content}{selectedNumber}";
+                    else
+                        ResultLbl.Content = $"{selectedNumber}";
                 }
-
             }
-
+            else
+            {
+                ResultLbl.Content = $"{ResultLbl.Content}{selectedNumber}";
+            }
         }
 
         private void PerformOperation()
@@ -154,24 +245,24 @@ namespace Calculator
             }
         }
 
-        private string GetSelectedOperator()
-        {
-            switch (_selectedOperator)
-            {
-                case SelectedOperator.Addition:
-                    return "+";
-                case SelectedOperator.Subtract:
-                    return "-";
-                case SelectedOperator.Multiply:
-                    return "*";
-                case SelectedOperator.Divide:
-                    return "/";
-                case SelectedOperator.Nothing:
-                    return "";
-                default:
-                    return "";
-            }
-        }
+        //private string GetSelectedOperator()
+        //{
+        //    switch (_selectedOperator)
+        //    {
+        //        case SelectedOperator.Addition:
+        //            return "+";
+        //        case SelectedOperator.Subtract:
+        //            return "-";
+        //        case SelectedOperator.Multiply:
+        //            return "*";
+        //        case SelectedOperator.Divide:
+        //            return "/";
+        //        case SelectedOperator.Nothing:
+        //            return "";
+        //        default:
+        //            return "";
+        //    }
+        //}
     }
 
     public enum SelectedOperator
