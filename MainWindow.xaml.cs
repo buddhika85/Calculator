7f24d70e300b573f25cc9da4c28191fd816d7a92
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-08-01 18:52:02 +0530

    fixes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,7 +9,8 @@ namespace Calculator
     public partial class MainWindow : Window
     {
         private SelectedOperator _selectedOperator = SelectedOperator.Nothing;
-        private double _calculatedResult = 0.0;
+        private double _calculatedValue = 0.0;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -20,6 +21,7 @@ namespace Calculator
         {
             ResultLbl.Content = string.Empty;
             ExpressionLbl.Content = string.Empty;
+            _calculatedValue = 0.0;
         }
 
 
@@ -32,7 +34,6 @@ namespace Calculator
         {
             if (double.TryParse(ResultLbl.Content.ToString(), out var result))
             {
-
                 ResultLbl.Content = result * -1;
             }
 
@@ -57,18 +58,116 @@ namespace Calculator
 
         private void OnOperationBtnClick(object sender, RoutedEventArgs e)
         {
-
+            var operation = GetBtnContent(sender);
+            if (_selectedOperator != SelectedOperator.Nothing)
+            {
+                ExpressionLbl.Content = $"{ExpressionLbl.Content}{ResultLbl.Content}";
+                PerformOperation();     // previous calculation
+            }
+            else
+            {
+                ExpressionLbl.Content = $"{ResultLbl.Content}{operation}";
+            }
+            SetSelectedOperator(operation);
         }
 
         private void OnNumberBtnClick(object sender, RoutedEventArgs e)
         {
-            var btnContent = ((Button)sender).Content.ToString();
-            if (int.TryParse(btnContent, out var selectedNumber))
+            var numberStr = GetBtnContent(sender);
+            if (int.TryParse(numberStr, out var selectedNumber))
             {
+                if (_selectedOperator != SelectedOperator.Nothing)
+                {
+                    if (_calculatedValue == 0.0)
+                    {
+                        double.TryParse(ResultLbl.Content.ToString(), out _calculatedValue);
+                        ResultLbl.Content = $"{selectedNumber}";
+                    }
+                    else
+                    {
+                        ResultLbl.Content = $"{ResultLbl.Content}{selectedNumber}";
+                    }
+                }
+                else
+                {
+                    ResultLbl.Content = $"{ResultLbl.Content}{selectedNumber}";
+                }
 
-                ResultLbl.Content = $"{ResultLbl.Content}{selectedNumber}";
             }
 
+        }
+
+        private void PerformOperation()
+        {
+            if (double.TryParse(ResultLbl.Content.ToString(), out var lastNumber))
+            {
+                switch (_selectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        _calculatedValue += lastNumber;
+                        break;
+                    case SelectedOperator.Subtract:
+                        _calculatedValue -= lastNumber;
+                        break;
+                    case SelectedOperator.Multiply:
+                        _calculatedValue *= lastNumber;
+                        break;
+                    case SelectedOperator.Divide:
+                        _calculatedValue /= lastNumber;
+                        break;
+                    case SelectedOperator.Nothing:
+                        break;
+                }
+            }
+            _selectedOperator = SelectedOperator.Nothing;
+            //ExpressionLbl.Content = lastNumber;
+            ResultLbl.Content = _calculatedValue;
+        }
+
+        private string? GetBtnContent(object sender)
+        {
+            return ((Button)sender).Content?.ToString();
+        }
+
+        private void SetSelectedOperator(string? operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    _selectedOperator = SelectedOperator.Addition;
+                    break;
+                case "-":
+                    _selectedOperator = SelectedOperator.Subtract;
+                    break;
+                case "*":
+                    _selectedOperator = SelectedOperator.Multiply;
+                    break;
+                case "/":
+                    _selectedOperator = SelectedOperator.Divide;
+                    break;
+                default:
+                    _selectedOperator = SelectedOperator.Nothing;
+                    break;
+            }
+        }
+
+        private string GetSelectedOperator()
+        {
+            switch (_selectedOperator)
+            {
+                case SelectedOperator.Addition:
+                    return "+";
+                case SelectedOperator.Subtract:
+                    return "-";
+                case SelectedOperator.Multiply:
+                    return "*";
+                case SelectedOperator.Divide:
+                    return "/";
+                case SelectedOperator.Nothing:
+                    return "";
+                default:
+                    return "";
+            }
         }
     }
 
